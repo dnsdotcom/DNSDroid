25b83e22aa038e21aa60af398f5227977b4cf889
Deven Phillips <deven@dns.com>
2011-12-18 23:23:28 -0500

    Stubbed in the GeoGroupsListActivity a little more.

--- src/com/dns/mobile/activities/geo/GeoGroupsListActivity.java
+++ src/com/dns/mobile/activities/geo/GeoGroupsListActivity.java
@@ -4,6 +4,8 @@
 package com.dns.mobile.activities.geo;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 
 /**
  * @author <a href="mailto:deven@dns.com">Deven Phillips</a>
@@ -12,4 +14,14 @@ import android.app.Activity;
 public class GeoGroupsListActivity extends Activity {
 
 	private static final String TAG = "GeoGroupsListActivity" ;
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		Log.d(TAG, "Starting the GeoGroupsListActivity") ;
+	}
 }
