f4f0551a589b1c5b0f54ed62bdcfdead842ed8e9
Deven Phillips <deven@dns.com>
2011-12-21 01:09:55 -0500

    Now have domainGroup hosts saving correctly, just need to fix up the domainGroup resource records.

--- src/com/dns/mobile/activities/records/CreateNewHostActivity.java
+++ src/com/dns/mobile/activities/records/CreateNewHostActivity.java
@@ -31,11 +31,92 @@ public class CreateNewHostActivity extends Activity {
 	protected boolean isExistingRecord = false ;
 
 	/**
+	 * @author <a href="mailto:deven@dns.com">Deven Phillips</a>
+	 *
+	 */
+	private final class RecordTypeSelectHandler implements
+			AdapterView.OnItemSelectedListener {
+		/* (non-Javadoc)
+		 * @see android.widget.AdapterView.OnItemSelectedListener#onItemSelected(android.widget.AdapterView, android.view.View, int, long)
+		 */
+		public void onItemSelected(AdapterView<?> recordTypeList, View selectedView, int position, long itemId) {
+
+			findViewById(R.id.rrAnswer).setVisibility(View.GONE) ;
+			findViewById(R.id.rrAnswerLabel).setVisibility(View.GONE) ;
+			findViewById(R.id.rrTtlInput).setVisibility(View.GONE) ;
+			findViewById(R.id.rrTtlLabel).setVisibility(View.GONE) ;
+			findViewById(R.id.rrPriority).setVisibility(View.GONE) ;
+			findViewById(R.id.rrPriorityLabel).setVisibility(View.GONE) ;
+			findViewById(R.id.rrSrvPortLabel).setVisibility(View.GONE) ;
+			findViewById(R.id.rrSrvPort).setVisibility(View.GONE) ;
+			findViewById(R.id.rrSrvWeightLabel).setVisibility(View.GONE) ;
+			findViewById(R.id.rrSrvWeight).setVisibility(View.GONE) ;
+			findViewById(R.id.rrExpire).setVisibility(View.GONE) ;
+			findViewById(R.id.rrExpireLabel).setVisibility(View.GONE) ;
+			findViewById(R.id.rrMinimum).setVisibility(View.GONE) ;
+			findViewById(R.id.rrMinimumLabel).setVisibility(View.GONE) ;
+			findViewById(R.id.rrRetryInterval).setVisibility(View.GONE) ;
+			findViewById(R.id.rrRetryLabel).setVisibility(View.GONE) ;
+			findViewById(R.id.rrPriority).setVisibility(View.GONE) ;
+			findViewById(R.id.rrPriorityLabel).setVisibility(View.GONE) ;
+			findViewById(R.id.rrResponsibleParty).setVisibility(View.GONE) ;
+			findViewById(R.id.rrResponsiblePartyLabel).setVisibility(View.GONE) ;
+			switch (ResourceRecord.getTypeForIdentifier((String)recordTypeList.getSelectedItem())) {
+				case 6:
+					// SOA Record
+					findViewById(R.id.rrTtlInput).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrTtlLabel).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrExpire).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrExpireLabel).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrMinimum).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrMinimumLabel).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrRetryInterval).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrRetryLabel).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrResponsibleParty).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrResponsiblePartyLabel).setVisibility(View.VISIBLE) ;
+					break ;
+				case 15:
+					findViewById(R.id.rrAnswer).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrAnswerLabel).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrTtlInput).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrTtlLabel).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrPriority).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrPriorityLabel).setVisibility(View.VISIBLE) ;
+					break ;
+				case 33:
+					findViewById(R.id.rrAnswer).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrAnswerLabel).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrTtlInput).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrTtlLabel).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrPriority).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrPriorityLabel).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrSrvPortLabel).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrSrvPort).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrSrvWeightLabel).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrSrvWeight).setVisibility(View.VISIBLE) ;
+					break ;
+				default:
+					findViewById(R.id.rrAnswer).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrAnswerLabel).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrTtlInput).setVisibility(View.VISIBLE) ;
+					findViewById(R.id.rrTtlLabel).setVisibility(View.VISIBLE) ;
+			}
+		}
+
+		/* (non-Javadoc)
+		 * @see android.widget.AdapterView.OnItemSelectedListener#onNothingSelected(android.widget.AdapterView)
+		 */
+		public void onNothingSelected(AdapterView<?> recordTypeList) {
+			recordTypeList.setSelection(0) ;
+		}
+	}
+
+	/**
 	 * A class which extends the AsyncTask AbstractClass to perform network operations in the background.
 	 * @author <a href="mailto:deven@dns.com">Deven Phillips</a>
 	 *
 	 */
-	private class RRSaveViaAPI extends AsyncTask<ResourceRecord, Void, JSONObject> {
+	private class RRSaveAdvancedViaAPI extends AsyncTask<ResourceRecord, Void, JSONObject> {
 
 		private int rrId = 0 ;
 
@@ -60,8 +141,6 @@ public class CreateNewHostActivity extends Activity {
 				useSSL = true ;
 				apiHost = "www.dns.com" ;
 			}
-
-			// TODO: Modify this activity to be able to save hosts for a domain group as well as normal domain.
 
 			ManagementAPI api = new ManagementAPI(apiHost, useSSL, settings.getString("auth.token", "")) ;
 
@@ -181,89 +260,17 @@ public class CreateNewHostActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.new_host_layout) ;
 		currentRR.setDomainName(this.getIntent().getStringExtra("domainName")) ;
+		currentRR.setGroup(this.getIntent().getBooleanExtra("isDomainGroup", false)) ;
 
 		domainName = this.getIntent().getStringExtra("domainName") ;
+		
 		findViewById(R.id.dnsLogo).setOnClickListener(new LogoOnClickListener(this));
 		Log.d(TAG, "domainName: "+domainName) ;
 		TextView newHostHeader = (TextView)findViewById(R.id.newHostHeader) ;
 		String hostHeaderContent = newHostHeader.getText().toString()+": "+domainName ;
 		newHostHeader.setText(hostHeaderContent) ;
 
-		((Spinner)findViewById(R.id.hostType)).setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-			/* (non-Javadoc)
-			 * @see android.widget.AdapterView.OnItemSelectedListener#onItemSelected(android.widget.AdapterView, android.view.View, int, long)
-			 */
-			public void onItemSelected(AdapterView<?> recordTypeList, View selectedView, int position, long itemId) {
-
-				findViewById(R.id.rrAnswer).setVisibility(View.GONE) ;
-				findViewById(R.id.rrAnswerLabel).setVisibility(View.GONE) ;
-				findViewById(R.id.rrTtlInput).setVisibility(View.GONE) ;
-				findViewById(R.id.rrTtlLabel).setVisibility(View.GONE) ;
-				findViewById(R.id.rrPriority).setVisibility(View.GONE) ;
-				findViewById(R.id.rrPriorityLabel).setVisibility(View.GONE) ;
-				findViewById(R.id.rrSrvPortLabel).setVisibility(View.GONE) ;
-				findViewById(R.id.rrSrvPort).setVisibility(View.GONE) ;
-				findViewById(R.id.rrSrvWeightLabel).setVisibility(View.GONE) ;
-				findViewById(R.id.rrSrvWeight).setVisibility(View.GONE) ;
-				findViewById(R.id.rrExpire).setVisibility(View.GONE) ;
-				findViewById(R.id.rrExpireLabel).setVisibility(View.GONE) ;
-				findViewById(R.id.rrMinimum).setVisibility(View.GONE) ;
-				findViewById(R.id.rrMinimumLabel).setVisibility(View.GONE) ;
-				findViewById(R.id.rrRetryInterval).setVisibility(View.GONE) ;
-				findViewById(R.id.rrRetryLabel).setVisibility(View.GONE) ;
-				findViewById(R.id.rrPriority).setVisibility(View.GONE) ;
-				findViewById(R.id.rrPriorityLabel).setVisibility(View.GONE) ;
-				findViewById(R.id.rrResponsibleParty).setVisibility(View.GONE) ;
-				findViewById(R.id.rrResponsiblePartyLabel).setVisibility(View.GONE) ;
-				switch (ResourceRecord.getTypeForIdentifier((String)recordTypeList.getSelectedItem())) {
-					case 6:
-						// SOA Record
-						findViewById(R.id.rrTtlInput).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrTtlLabel).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrExpire).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrExpireLabel).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrMinimum).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrMinimumLabel).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrRetryInterval).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrRetryLabel).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrResponsibleParty).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrResponsiblePartyLabel).setVisibility(View.VISIBLE) ;
-						break ;
-					case 15:
-						findViewById(R.id.rrAnswer).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrAnswerLabel).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrTtlInput).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrTtlLabel).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrPriority).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrPriorityLabel).setVisibility(View.VISIBLE) ;
-						break ;
-					case 33:
-						findViewById(R.id.rrAnswer).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrAnswerLabel).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrTtlInput).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrTtlLabel).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrPriority).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrPriorityLabel).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrSrvPortLabel).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrSrvPort).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrSrvWeightLabel).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrSrvWeight).setVisibility(View.VISIBLE) ;
-						break ;
-					default:
-						findViewById(R.id.rrAnswer).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrAnswerLabel).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrTtlInput).setVisibility(View.VISIBLE) ;
-						findViewById(R.id.rrTtlLabel).setVisibility(View.VISIBLE) ;
-				}
-			}
-
-			/* (non-Javadoc)
-			 * @see android.widget.AdapterView.OnItemSelectedListener#onNothingSelected(android.widget.AdapterView)
-			 */
-			public void onNothingSelected(AdapterView<?> recordTypeList) {
-				recordTypeList.setSelection(0) ;
-			}
-		}) ;
+		((Spinner)findViewById(R.id.hostType)).setOnItemSelectedListener(new RecordTypeSelectHandler()) ;
 
 		Button saveButton = (Button) findViewById(R.id.rrSaveButton) ;
 		saveButton.setOnClickListener(new View.OnClickListener() {
@@ -294,7 +301,7 @@ public class CreateNewHostActivity extends Activity {
 						break ;
 						
 				}
-				new RRSaveViaAPI().execute(currentRR) ;
+				new RRSaveAdvancedViaAPI().execute(currentRR);
 			}
 		}) ;
 	}
